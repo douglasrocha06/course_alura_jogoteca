adc329c8ed61b0d6fb8ce8131955b20b8dc96003
Douglas <douglas.rocha@inmetrics.com.br>
2021-10-15 13:55:37 -0300

    upload files

---
+++ jogoteca.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_mysqldb import MySQL
+
+#Instanciando o objeto Flask
+app = Flask(__name__)
+app.config.from_pyfile('config.py')
+
+db = MySQL(app)
+
+from views import *
+
+if __name__ == '__main__':
+    app.run(debug=True)
